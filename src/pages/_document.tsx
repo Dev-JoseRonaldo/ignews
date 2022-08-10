ec67d52a7298d3af56ca57803ee5818a9b2608c8
José Ronaldo <dev.joseronaldo@gmail.com>
2022-08-09 22:42:57 -0300

    :sparkles: feat: Estrutura e estilização do component Header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,10 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com"  />
+          <link rel="icon" href="/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-    
+
+          
         </Head>
         <body>
           <Main />
