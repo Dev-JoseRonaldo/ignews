532990fd6a087e82f2c954252b020f62a2616ea2
José Ronaldo <dev.joseronaldo@gmail.com>
2022-08-23 22:59:13 -0300

    :sparkles: feat: Estrutura e estilização do botão de Sign In Github

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com"  />
           <link rel="icon" href="/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-
-          
+      
         </Head>
         <body>
           <Main />
