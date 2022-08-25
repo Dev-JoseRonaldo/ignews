2b1474a74ba75d6c20a4cd68352bb1870db49734
José Ronaldo <dev.joseronaldo@gmail.com>
2022-08-24 22:11:55 -0300

    :sparkles: feat: Salvando usuário no banco Faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb"
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -7,5 +11,25 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
+
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile}) {
+      const { email } = user
+
+      try{
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true
+      }
+      catch{
+        return false
+      }
+    },
+  } 
 })
