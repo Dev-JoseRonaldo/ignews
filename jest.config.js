abce6229a803c577976428a84bd7654693e3ba7e
José Ronaldo <dev.joseronaldo@gmail.com>
2022-09-07 21:40:22 -0300

    :heavy_plus_sign: build: Configurando Testing Library

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testIgnorePatterns: ['/node_modules/', '/.next/'],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTest.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+  testEnvironment: 'jsdom'
+}
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,17 @@
 import { SignInButton } from '../SignInButton';
-
+import Image from 'next/image'
 import styles from './styles.module.scss';
 
 export function Header() {
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Image 
+          src="/images/logo.svg"           
+          alt="ig.news" 
+          width={110} 
+          height={30} 
+        />
         <nav>
           <a className={styles.active} href="">Home</a>
           <a href="">Posts</a>
